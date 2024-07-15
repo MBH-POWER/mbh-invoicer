8307aeeb2e9e3b78714135b94d32b952423319ec
sijirama <gbemilesanmi@gmail.com>
2024-07-15 09:37:52 +0100

    vercel errors

--- app/(authentication)/signout/page.tsx
+++ app/(authentication)/signout/page.tsx
@@ -2,7 +2,7 @@
 import { useAuth } from '@/store/authStore'
 import { useRouter } from 'next/navigation'
 
-export default function page() {
+export default function Page() {
 
     const router = useRouter()
     const { user, logOut } = useAuth()
